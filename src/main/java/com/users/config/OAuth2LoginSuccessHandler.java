c4d48e06c32838ce8428c8f2dc75d669d87c9662
Kenneth Milota <kenm661@gmail.com>
2026-03-09 22:33:35 -0400

    OAuth2LoginSuccessHandler added to user service

---
+++ src/main/java/com/users/config/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,34 @@
+package com.users.config;
+
+import com.users.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserService userService;
+
+    public OAuth2LoginSuccessHandler(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication)
+            throws IOException, ServletException {
+
+        OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+        userService.registerOrUpdateUser(oauthUser.getAttributes());
+
+        response.sendRedirect("/profile");
+    }
+}
